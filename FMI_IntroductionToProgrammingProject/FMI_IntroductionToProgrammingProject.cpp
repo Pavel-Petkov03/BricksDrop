b8c39f105452be133fd15cec8ae2607b1b618b0d
Pavel Petkov <elias5247698@abv.bg>
2023-12-18 13:00:35 +0200

    removing string lib

--- FMI_IntroductionToProgrammingProject/FMI_IntroductionToProgrammingProject.cpp
+++ FMI_IntroductionToProgrammingProject/FMI_IntroductionToProgrammingProject.cpp
@@ -1,7 +1,6 @@
 // FMI_IntroductionToProgrammingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <windows.h>
 using namespace std;
