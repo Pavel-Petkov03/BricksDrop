2218c0976a3cf9e89b57f1c96c0250d1935e95fb
Pavel Petkov <elias5247698@abv.bg>
2024-01-22 20:49:10 +0200

    Updating personal info

--- FMI_IntroductionToProgrammingProject/FMI_IntroductionToProgrammingProject.cpp
+++ FMI_IntroductionToProgrammingProject/FMI_IntroductionToProgrammingProject.cpp
@@ -1,5 +1,18 @@
-// FMI_IntroductionToProgrammingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/**
+*  
+* Solution to course project # <номер на вариант>
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Pavel Petkov
+* @idnumber 0MI0600302
+* @compiler VC
+*
+* 
+*
+*/
+
 #include <iostream>
 #include <fstream>
 #include <windows.h>
